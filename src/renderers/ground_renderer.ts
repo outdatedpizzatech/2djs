c80db67f3dafa1d0e5db6f04fcad477b2a5ca7a5
Eric Iseneker <erici@carbonfive.com>
2020-10-08 11:43:32 -0400

    Infinite terrain

--- src/renderers/ground_renderer.ts
+++ src/renderers/ground_renderer.ts
@@ -15,7 +15,32 @@ export const renderGround = (
     "repeat"
   ) as CanvasPattern;
 
-  bufferCtx.translate(groundOffsetX, groundOffsetY);
-  bufferCtx.fillRect(0, 0, CAMERA_WIDTH * 2, CAMERA_HEIGHT * 2);
-  bufferCtx.translate(-groundOffsetX, -groundOffsetY);
+  const modX = groundOffsetX % CAMERA_WIDTH;
+  const modY = groundOffsetY % CAMERA_HEIGHT;
+  const formulaX = modX - (CAMERA_WIDTH * Math.abs(modX)) / modX;
+  const formulaY = modY - (CAMERA_HEIGHT * Math.abs(modY)) / modY;
+
+  bufferCtx.fillRect(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+
+  bufferCtx.translate(modX, modY);
+  bufferCtx.fillRect(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+  bufferCtx.translate(-modX, -modY);
+
+  if (modX != 0) {
+    bufferCtx.translate(formulaX, modY);
+    bufferCtx.fillRect(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    bufferCtx.translate(-formulaX, -modY);
+  }
+
+  if (modY != 0) {
+    bufferCtx.translate(modX, formulaY);
+    bufferCtx.fillRect(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    bufferCtx.translate(-modX, -formulaY);
+  }
+
+  if (modY !== 0 && modX !== 0) {
+    bufferCtx.translate(formulaX, formulaY);
+    bufferCtx.fillRect(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    bufferCtx.translate(-formulaX, -formulaY);
+  }
 };
